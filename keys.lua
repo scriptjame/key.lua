2a8cad977e8901b12da141f0ff3607f7c874c186
scriptjame <132070412+scriptjame@users.noreply.github.com>
2026-03-12 02:53:08 +0700

    Create keys.lua

---
+++ keys.lua
@@ -0,0 +1,12 @@
+return {
+"grtnjkn4aewas49",
+"x91ks8a2lq0pw3",
+"a8f2n4s9x0kq7",
+"k9s2n1p4x0q8a7",
+"p2a9x7n0k4s1q8",
+"z8k2x1n4p7a0q9",
+"f7a9p0k1x2n4s8",
+"q2n7a4k9p1x0s8",
+"s1p9a0n7k2x4q8",
+"x4k1p8n9a0q7s2"
+}
